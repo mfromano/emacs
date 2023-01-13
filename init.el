7a8844fa838722ba1b37dcf2baec084454b5b3ef
Michael Romano <michaelfromano@gmail.com>
2023-01-13 12:43:10 -0800

    updated things...

--- init.el
+++ init.el
@@ -5,9 +5,29 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" default))
+ '(org-capture-templates
+   '(("t" "todo" entry
+      (file+headline "/Users/mromano/Sync/todo.org" "Tasks")
+      "* TODO [#A] %?")
+     ("j" "ResearchJournal" entry
+      (file+olp+datetree "/Users/mromano/Sync/Notebook/research/journal.org")
+      "* %?
+Entered on %U
+  %i
+  %a")
+     ("n" "RadiologyNotes" entry
+      (file+olp+datetree "/Users/mromano/Sync/Notebook/radiology/notes.org")
+      "* %?
+Entered on %U
+  %i
+  %a")
+     ("b" "Journal" entry
+      (file "/Users/mromano/Sync/Notebook/blog.org")
+      "* %?
+	    Entered on %U")))
  '(org-export-backends '(ascii html icalendar latex md odt))
  '(package-selected-packages
-   '(jupyter ein conda tramp tramp-term helm-tramp hippie-exp-ext which-key helm-lsp helm-fuzzy helm-org use-package-chords markdown-mode pdf-tools solarized-theme smartparens python eglot)))
+   '(evil ess python-mode pylint poly-markdown js2-mode gh anki-vocabulary anki-editor org-bullets anki-connect anki-mode ac-helm jupyter ein conda tramp tramp-term helm-tramp which-key helm-lsp helm-fuzzy helm-org use-package-chords markdown-mode pdf-tools solarized-theme smartparens python eglot)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,7 +36,6 @@
  )
 
 (require 'package)
-(require 'org)
 
 ;; Add melpa package source when using package list
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
@@ -27,11 +46,10 @@
 ;; Don't delete this line.
 (package-initialize)
 
-(require 'markdown-mode)
+(require 'org)
 (require 'helm-org)
+(require 'helm)
 (require 'python)
-(require 'hippie-exp-ext)
-(require 'helm-fuzzy)
 (require 'smartparens)
 (require 'use-package)
 (require 'tramp)
@@ -39,15 +57,33 @@
 (require 'ein)
 (require 'conda)
 
-;; disable splash
 (setq inhibit-splash-screen t)
-
+(tool-bar-mode -1)
 (setq tramp-default-method "ssh")
 
-(load-theme 'solarized-dark t)
+(when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
+(setq initial-scratch-message "")
 
-;; enable transient mark mode
-(transient-mark-mode 1)
+;; APPEARANCE
+(load-theme 'solarized-dark t)
+(set-face-attribute 'default (selected-frame) :height 150)
+
+;; modes 
+(setq-default word-wrap t)
+(global-visual-line-mode t)
+(which-key-mode t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . jekyll-markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . jekyll-html-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'auto-mode-alist '("\\.ipynb\\'" . ein:ipynb-mode))
+(add-to-list 'auto-mode-alist '("\\.r\\'" . ess-r-mode))
+
+(auto-complete-mode 1)
+
+;; custom functions for SSH
 
 (defun manu ()
   (interactive)
@@ -73,40 +109,31 @@
   (interactive)
   (shell-command "bash $HOME/research/utils/new_journal_date.sh"))
 
+;; Shortcuts
 (global-set-key (kbd "C-c m") 'manu)
 (global-set-key (kbd "C-c w d") 'wyndev)
 (global-set-key (kbd "C-c w w") 'wyn)
-
-;; from pragmaticemacs.wordpress.com
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
 
-(setq org-agenda-files (quote
-  ("/Users/mromano/.spacemacs.d/todo.org"
-   "/Users/mromano/Google Drive/My Drive/Notebook/research/")))
 
+;; ORG MODE PREFERENCES
+(setq org-agenda-files
+  (quote ("/Users/mromano/Sync/todo.org"
+   "/Users/mromano/Sync/Notebook/research"
+   "/Users/mromano/Sync/Notebook/radiology"
+   "/Users/mromano/Sync/Notebook/blog.org"
+)))
 (setq org-highest-priority ?A)
 (setq org-lowest-priority ?C)
 (setq org-default-priority ?A)
-
-(setq org-priority-faces '((?A . (:foreground
-"#F0DFAF" :weight bold))
-                         (?B . (:foreground
-"LightSteelBlue"))
-                         (?C . (:foreground
-"OliveDrab"))))
-
-(setq org-agenda-window-setup (quote current-window))
-
-(define-key global-map (kbd "C-c c") 'org-capture)
-
-(setq org-capture-templates
-      '(("t" "todo" entry (file+headline "/Users/mromano/.spacemacs.d/todo.org" "Tasks") "* TODO [#A] %?")
-      ("j" "Journal" entry (file+datetree "/Users/mromano/Google Drive/My Drive/Notebook/research/journal.org") "* %?\nEntered on %U\n  %i\n  %a")
-      ("n" "Journal" entry (file+datetree "/Users/mromano/Google Drive/My Drive/Notebook/radiology/notes.org") "* %?\nEntered on %U\n  %i\n  %a"))
-      )
-
-(setq-default word-wrap t)
-(global-visual-line-mode t)
+(setq org-priority-faces
+   '((?A . (:foreground "#F0DFAF" :weight bold))
+     (?B . (:foreground "LightSteelBlue"))
+     (?C . (:foreground "OliveDrab"))))
+(setq org-agenda-window-setup 'current-window)
 
 (use-package pdf-tools
   :pin manual
@@ -119,13 +146,12 @@
   (require 'pdf-tools)
   )
 
-
  ;; config lines below from zzamboni.org/post, "How to insert screenshots in Org documents on macOS"
 (use-package org-download
   :after org
   :defer nil
   :custom
-  (org-download-method 'directory)
+  (org-download-method 'attach)
   (ord-download-image-dir "images")
   (org-download-heading-lvl nil)
   (org-download-timestamp "%Y%m%d-%H%M%S_")
@@ -135,30 +161,3 @@
   ("C-M-y" . org-download-screenshot)
   :config
   (require 'org-download))
-
-(when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
-
-;; Do not write anything past this comment. This is where Emacs will
-;; auto-generate custom variable definitions.
- '(org-capture-templates
-   '(("t" "todo" entry
-      (file+headline "/Users/mromano/.spacemacs.d/todo.org" "Tasks")
-      "* TODO [#A] %?")
-     ("j" "ResearchJournal" entry
-      (file+olp+datetree "/Users/mromano/Google Drive/My Drive/Notebook/research/journal.org")
-      "* %?
-Entered on %U
-  %i
-  %a")
-     ("n" "RadiologyNotes" entry
-      (file+olp+datetree "/Users/mromano/Google Drive/My Drive/Notebook/radiology/notes.org")
-      "* %?
-Entered on %U")
-     ("g" "RadioGraphics" entry
-      (file "/Users/mromano/Google Drive/My Drive/Notebook/radiology/rg.org")
-      "* %? %^g
-Entered on %U")))
-
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(setq initial-scratch-message "")
